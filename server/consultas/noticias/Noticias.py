cf7042d3126a9101527869517912c7f02169db29
OtilianoJunior <otiliano.rodrigues@veri.com.br>
2023-03-03 15:52:11 -0300

    refactor: code

--- server/consultas/noticias/Noticias.py
+++ server/consultas/noticias/Noticias.py
@@ -1,5 +1,4 @@
-from newspaper import Article, Config
-from newspaper import fulltext
+from newspaper import Article
 import requests
 from bs4 import BeautifulSoup
 
@@ -8,43 +7,55 @@ class NoticiasConsulta:
     def __init__(self, url):
         self.url = url
 
-    def get_article_info(self, link):
-        article = Article(link)
-        article.download()
-        article.parse()
-
-        titulo = article.title
-        data_publicacao = article.publish_date
-        autores = article.authors
-        texto = article.text
-        imagens = article.images
-
-        noticia = {
-            'titulo': titulo,
-            'data_publicacao': str(data_publicacao),
-            'autores': autores,
-            'texto': texto,
-            'imagens': imagens
-        }
-
-        return noticia
-
     def noticias(self):
         try:
-            response = requests.get(self.url)
-            soup = BeautifulSoup(response.content, 'html.parser')
-            links = [a['href'] for a in soup.find_all('a', href=True)]
+            links = self.get_links()
             noticias = []
 
             for link in links:
                 if 'http' in link:
                     try:
-                        noticia = self.get_article_info(link)
+                        noticia = self.get_info_noticia(link)
                         noticias.append(noticia)
                     except:
                         pass
 
             return noticias
+        except Exception as ex:
+            print(ex)
+            raise Exception
+
+    def get_links(self):
+        try:
+            response = requests.get(self.url)
+            soup = BeautifulSoup(response.content, 'html.parser')
+            links = [a['href'] for a in soup.find_all('a', href=True)]
+            return links
+        except Exception as ex:
+            print(ex)
+            raise Exception
+
+    def get_info_noticia(self, link):
+        try:
+            article = Article(link)
+            article.download()
+            article.parse()
+
+            titulo = article.title
+            data_publicacao = article.publish_date
+            autores = article.authors
+            texto = article.text
+            imagens = article.images
+
+            noticia = {
+                'titulo': titulo,
+                'data_publicacao': str(data_publicacao),
+                'autores': autores,
+                'texto': texto,
+                'imagens': imagens
+            }
+
+            return noticia
         except Exception as ex:
             print(ex)
             raise Exception
