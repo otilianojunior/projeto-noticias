cb1f48b15cb87429b1d3fa492cab84b29e825098
OtilianoJunior <otiliano.rodrigues@veri.com.br>
2023-03-07 15:22:37 -0300

    feat: apache-airflow em andamento

--- apache_airflow/dags/dag_api_noticias.py
+++ apache_airflow/dags/dag_api_noticias.py
@@ -1,22 +1,34 @@
+import os
 import requests
 from airflow.models import DAG
 from datetime import datetime, timedelta
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
+import subprocess
 
 
 def start_api():
-    # Execute o script que inicia a API aqui
-    # Exemplo: python /caminho/para/script/start_api.py
-    pass
+    command = "python /projeto-noticias/app/main.py"
+    process = subprocess.Popen(command.split())
+    return process.pid
 
 
-def requisicao_api_noticias():
-    # Execute a requisição da API aqui
-    response = requests.get('https://www.bbc.com/portuguese/topics/c404v027pd4t')
+def stop_api(pid):
+    os.system(f"kill {pid}")
 
-    # Exiba o conteúdo da resposta da API
-    print(response.content)
+
+def requisicao_api_noticias(urls):
+    for url in urls:
+        response = None
+        try:
+            response = requests.get('http://0.0.0.0:8000/api_noticias/diariamente', params={'url': url}, timeout=10)
+        except requests.exceptions.Timeout:
+            pass
+        except requests.exceptions.RequestException as e:
+            print(f"Erro ao fazer requisição para a URL {url}: {str(e)}")
+
+        if response is not None:
+            print(response.content)
 
 
 default_args = {
@@ -30,20 +42,30 @@ default_args = {
 dag = DAG(
     'dag_api_noticias',
     default_args=default_args,
-    description='DAG que executa a API uma vez por dia',
-    schedule_interval=timedelta(days=1),
+    description='DAG que executa a API a cada hora',
+    schedule_interval=timedelta(hours=1),
 )
 
-start_operator = BashOperator(
+start_operator = PythonOperator(
     task_id='start_api',
-    bash_command='python /caminho/para/script/start_api.py',
-    dag=dag
-)
-
-my_operator = PythonOperator(
-    task_id='my_task',
-    python_callable=requisicao_api_noticias,
+    python_callable=start_api,
     dag=dag,
 )
 
-start_operator >> my_operator
+urls = ['https://www.bbc.com/portuguese/topics/c404v027pd4t', 'https://www.bbc.com/portuguese/topics/cz74k717pw5t']
+
+requisicao_api_noticias = PythonOperator(
+    task_id='requisicao_api_noticias',
+    python_callable=requisicao_api_noticias,
+    op_kwargs={'urls': urls},
+    dag=dag,
+)
+
+stop_operator = PythonOperator(
+    task_id='stop_api',
+    python_callable=stop_api,
+    op_kwargs={'pid': '{{ ti.xcom_pull(task_ids="start_api") }}'},
+    dag=dag,
+)
+
+start_operator >> requisicao_api_noticias >> stop_operator
